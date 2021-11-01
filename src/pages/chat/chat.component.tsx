5576ee6c3200d4babcc2aefb971c41dcd830961e
Andrei M <andreimartinenco.dev@gmail.com>
2021-11-01 01:18:26 -0400

    client socketio implementation | error handling (wip)

--- src/pages/chat/chat.component.tsx
+++ src/pages/chat/chat.component.tsx
@@ -1,9 +1,11 @@
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import ChatAppBar from '../../components/chat-app/chat-app-bar/chat-app-bar.component';
 import ChatDirectCall from '../../components/chat-app/chat-direct-call/chat-direct-call.component';
 import ChatUsers from '../../components/chat-app/chat-users/chat-users.component';
+import { RootState } from '../../redux/store';
 import { connectWithWebSocket } from '../../utils/webSocketConnection/webSocketConnection.service';
 import chatStyles from './chat.styles';
 
@@ -11,6 +13,9 @@ const Chat: React.FC = () => {
 
   const classes = chatStyles();
 
+  const user = useSelector((state: RootState) => state.user.user);
+
+  
   useEffect(()=>{
     connectWithWebSocket();
   },[]);
--- src/utils/webSocketConnection/webSocketConnection.service.ts
+++ src/utils/webSocketConnection/webSocketConnection.service.ts
@@ -1,4 +1,6 @@
 import socketClient, { Socket } from 'socket.io-client';
+import { signOutStart } from '../../redux/auth/auth.actions';
+import store from '../../redux/store';
 
 const SERVER = 'http://localhost:5000';
 
@@ -12,5 +14,32 @@ export const connectWithWebSocket = () => {
   socket.on('connection', () => {
     console.log('succesfully connected with websocket server ');
     console.log(socket.id);
+  });
+
+  socket.on('connect_error', err => {
+    
+    // useDispatch(signOutStart());
+    console.log(err)
+    store.dispatch(signOutStart());
+  });
+
+  socket.io.on('error', () => {
+    console.log('error');
+    console.log("Sorry, there seems to be an issue with the connection!");
+  });
+
+  socket.io.on("reconnect_error", (error) => { 
+    console.log('reconnect_error');
+    console.log("Sorry, there seems to be an issue with the connection!");
+  });
+
+  socket.io.on("reconnect_failed", () => {
+    console.log('reconnect_failed');  
+    console.log("Sorry, there seems to be an issue with the connection!");
+  });
+
+  socket.io.on("reconnect_attempt", (attempt) => {
+    console.log('reconnect_attempt');
+    console.log("Sorry, there seems to be an issue with the connection!");
   });
 }
